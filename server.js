82081c2977a95fb244e09f48a71878f273ec5a2d
sunnykxj1989 <sunnykxj55@gmail.com>
2026-03-17 21:00:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 npm init -y
 npm install express body-parser twilio
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  const { name, mobile, message } = req.body;
+
+  console.log(name, mobile, message);
+
+  // Save to database (you can use MongoDB)
+
+  res.send("Request Submitted Successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
